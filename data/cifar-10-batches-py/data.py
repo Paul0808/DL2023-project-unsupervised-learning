dfadacc0f7ab25d337a497a4188d6c9a65b83f21
rares1609 <79575613+rares1609@users.noreply.github.com>
2023-03-22 18:30:25 +0100

    normalized data

--- data/cifar-10-batches-py/data.py
+++ data/cifar-10-batches-py/data.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import numpy as np
 
 
 def unpickle(file):
@@ -46,5 +47,74 @@ def get_semisupervised_data(no_batches:int = 1):
 
     return training_labels, labeled_data, unlabeled_data
 
-training_labels, labeled_data, unlabeled_data = get_semisupervised_data()
-print(len(training_labels),len(labeled_data), len(unlabeled_data))
+def get_mean_stdev_training_data():
+    batch1, batch2, batch3, batch4, batch5 = get_training_data()
+    mean_list = []
+    std_list = []
+
+    for i in batch1[b'data']:
+        mean_list.append(np.mean(i))
+        std_list.append(np.std(i))
+
+    for i in batch2[b'data']:
+        mean_list.append(np.mean(i))
+        std_list.append(np.std(i))
+
+    for i in batch3[b'data']:
+        mean_list.append(np.mean(i))
+        std_list.append(np.std(i))
+
+    for i in batch4[b'data']:
+        mean_list.append(np.mean(i))
+        std_list.append(np.std(i))
+
+    for i in batch5[b'data']:
+        mean_list.append(np.mean(i))
+        std_list.append(np.std(i))
+
+
+    mean_training = np.mean(mean_list)
+    std_training = np.std(std_list)
+
+    return mean_training, std_training
+    
+
+
+def normalize_semisupervised_data():
+    _, labeled_data, unlabeled_data = get_semisupervised_data()
+
+    mean_training, std_training = get_mean_stdev_training_data()
+
+    for i in range(len(labeled_data)):
+        labeled_data[i] = [(float(j) - mean_training) / std_training for j in labeled_data[i]]
+
+    for i in range(len(unlabeled_data)):
+        unlabeled_data[i] = [(float(j) - mean_training) / std_training for j in unlabeled_data[i]]
+
+    return labeled_data, unlabeled_data
+
+def normalize_unsupervised_data():
+    unsupervised_data = get_unsupervised_data()
+    #unsupervised_data_normalized = [float(i) / sum(unsupervised_data) for i in unsupervised_data]
+
+    mean_training, std_training = get_mean_stdev_training_data()
+
+    for i in range(len(unsupervised_data)):
+        unsupervised_data[i] = [(float(j) - mean_training) / std_training for j in unsupervised_data[i]]
+
+    return unsupervised_data
+
+def normalize_test_data():
+    _, test_data = get_test_data()
+
+    mean_training, std_training = get_mean_stdev_training_data()
+
+    for i in len(test_data):
+        test_data[i] = [(float(j) - mean_training) / std_training for j in test_data[i]]
+
+    return test_data
+
+
+labeled_data, unlabeled_data = normalize_semisupervised_data()
+
+print(labeled_data)
